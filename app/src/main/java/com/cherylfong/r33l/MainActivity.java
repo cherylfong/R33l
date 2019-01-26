d50dea50af06b05417c8dabbdaff53e7aa224c69
cherylfong <42821602+cherylfong@users.noreply.github.com>
2019-01-26 09:58:11 -0800

    implement recyclerview

--- app/src/main/java/com/cherylfong/r33l/MainActivity.java
+++ app/src/main/java/com/cherylfong/r33l/MainActivity.java
@@ -2,8 +2,11 @@ package com.cherylfong.r33l;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.cherylfong.r33l.Adapters.MovieListAdapter;
 import com.cherylfong.r33l.Objects.Movie;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -19,11 +22,11 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
-
     private static final String API_KEY = "api_key=3cb75c31ee53a94adb2a217c345c85b6";
     // private static final String API_KEY = "api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"; //test key
-
     private static final String NOW_PLAYING_REQ = "https://api.themoviedb.org/3/movie/now_playing?";
+
+    private MovieListAdapter mAdapter;
 
     ArrayList<Movie> moviesNowPlaying;
 
@@ -32,13 +35,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView movieListRV;
+        movieListRV = this.findViewById(R.id.playing_now_movies_view);
+        // vertical scrolling
+        movieListRV.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
 
+        moviesNowPlaying = new ArrayList<>();
+
+        // populate the recyclerview
+        mAdapter = new MovieListAdapter(this, moviesNowPlaying);
+        movieListRV.setAdapter(mAdapter);
+
+        // to handle JSON objects
         AsyncHttpClient client = new AsyncHttpClient();
         client.get(NOW_PLAYING_REQ+API_KEY, new JsonHttpResponseHandler(){
-
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-
                 try {
 
                     // results is a an array of JSON Objects
@@ -46,7 +58,11 @@ public class MainActivity extends AppCompatActivity {
 
                     Log.d(LOG_TAG, "results JSON array: " + resultsJArray.toString());
 
-                    moviesNowPlaying = Movie.createMovieList(resultsJArray);
+                    // To maintain reference to movieNowPlaying for mAdapter line 46
+                    // moviesNowPlaying = Movie.createMovieList(resultsJArray);
+                    // possible to move lines 46 & 47 to bottom of onCreate & remove line 43
+                    moviesNowPlaying.addAll(Movie.createMovieList(resultsJArray));
+                    mAdapter.notifyDataSetChanged();
 
                     Log.d(LOG_TAG, "moviesNowPlaying arraylist: " + moviesNowPlaying.toString());
 
@@ -55,9 +71,7 @@ public class MainActivity extends AppCompatActivity {
                     e.printStackTrace();
                     Log.d(LOG_TAG, "Error: " + e.getMessage());
                 }
-
             }
-
             @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
                 super.onFailure(statusCode, headers, throwable, errorResponse);
