fc1a1d31c69ee9a71b0832b0716a6c56edee649c
cherylfong <42821602+cherylfong@users.noreply.github.com>
2019-01-27 16:05:59 -0800

    add progress bar to movie graphic when loading

--- app/src/main/java/com/cherylfong/r33l/Adapters/MovieListAdapter.java
+++ app/src/main/java/com/cherylfong/r33l/Adapters/MovieListAdapter.java
@@ -3,16 +3,30 @@ package com.cherylfong.r33l.Adapters;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.database.Cursor;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.Priority;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 import com.cherylfong.r33l.Objects.Movie;
 import com.cherylfong.r33l.R;
 
@@ -65,6 +79,7 @@ public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.Movi
         return movieArrayList.size();
     }
 
+
     /**
      * Inner class to hold the views needed to display a single item in the recycler-view
      */
@@ -74,8 +89,7 @@ public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.Movi
         TextView releaseDateTV;
         TextView overviewTV;
         ImageView graphicIV;
-
-
+        ProgressBar progressBar;
 
         /**
          * Constructor for ViewHolder. Within this constructor, get a reference to
@@ -92,11 +106,12 @@ public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.Movi
             releaseDateTV = itemView.findViewById(R.id.releaseDate_textView);
             overviewTV = itemView.findViewById(R.id.overview_textView);
             graphicIV = itemView.findViewById(R.id.movieGraphic_imageView);
-
+            progressBar = itemView.findViewById(R.id.progressBar);
         }
 
         // binds the values to the corresponding layout components
         public void bindValues(Movie m) {
+
 
             titleTV.setText(m.getTitle());
             releaseDateTV.setText(m.getReleaseDate());
@@ -108,8 +123,23 @@ public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.Movi
                 graphicURL = m.getBackdropPath();
             }
 
-            // loads the image from url to an ImageView
-            Glide.with(mContext).load(graphicURL).into(graphicIV);
+            // shows progress bar when movie image is loading
+            Glide.with(mContext)
+                    .load(graphicURL)
+                    .listener(new RequestListener<Drawable>() {
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                            progressBar.setVisibility(View.VISIBLE);
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            progressBar.setVisibility(View.GONE);
+                            return false;
+                        }
+                    })
+                    .into(graphicIV);
         }
     }
 }
