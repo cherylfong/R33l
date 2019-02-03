de4cb5423bd019d04157008b65616c2b38cb216f
cherylfong <42821602+cherylfong@users.noreply.github.com>
2019-02-03 13:52:36 -0800

    add implementation to replace YouTube player when bad request, empty result or missing youtube key

--- app/src/main/java/com/cherylfong/r33l/MovieDetailsActivity.java
+++ app/src/main/java/com/cherylfong/r33l/MovieDetailsActivity.java
@@ -1,11 +1,22 @@
 package com.cherylfong.r33l;
 
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.cherylfong.r33l.Objects.Movie;
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
@@ -35,6 +46,8 @@ public class MovieDetailsActivity extends YouTubeBaseActivity {
     TextView titleTV, overviewTV, releaseDateTV;
     RatingBar ratingBar;
     YouTubePlayerView youTubePlayerView;
+    ImageView noVideoIV;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,12 +82,21 @@ public class MovieDetailsActivity extends YouTubeBaseActivity {
 
         youTubePlayerView = findViewById(R.id.movie_youtube_detail_player);
 
+        progressBar = findViewById(R.id.replace_image_progressbar_detail_imageView);
+
+        noVideoIV = findViewById(R.id.no_trailer_detail_imageView);
+
         // to handle JSON objects
         AsyncHttpClient client = new AsyncHttpClient();
 
         Log.d(LOG_TAG, "ID: "+ m.getId());
 
         String movieVideoInfoURL = MOVIE_VIDEO_KEY_REQ + m.getId() + "/videos?" + MOVIEDB_API_KEY;
+
+        // TODO remove: for demo purpose
+        if(m.getId() == 504172 ){
+           movieVideoInfoURL = "https://api.themoviedb.org/3/movie/209312/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+        }
 
         Log.d(LOG_TAG, "movieVideoInfoURL: "+ movieVideoInfoURL );
 
@@ -87,14 +109,30 @@ public class MovieDetailsActivity extends YouTubeBaseActivity {
                 try {
 
                     JSONArray resultsJArray = response.getJSONArray("results");
+
                     if(resultsJArray.length() == 0){
 
-                        // TODO add placeholder if no trailer found!
-                        return; // incase if it is empty
+                        // To test this if statement
+                        // String movieVideoInfoURL = "https://api.themoviedb.org/3/movie/209312/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+                        //
+                        Log.d(LOG_TAG, "results zero length");
+                        replaceYouTubeVideo();
+                        return;
                     }
+
                     JSONObject movieVideoJSONObject = resultsJArray.getJSONObject(0); // only 1 json object returned
+
+                    // check if key is for YouTube
+                    if( !movieVideoJSONObject.getString("site").equals("YouTube") ){
+
+                        Log.d(LOG_TAG, "site != YouTube");
+                        replaceYouTubeVideo();
+                        return;
+                    }
+
                     String videoKey = movieVideoJSONObject.getString("key");
                     youTubePlayerInitializer(videoKey);
+                    youTubePlayerView.setVisibility(View.VISIBLE);
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -104,9 +142,10 @@ public class MovieDetailsActivity extends YouTubeBaseActivity {
 
             @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                super.onFailure(statusCode, headers, throwable, errorResponse);
-            }
 
+                Log.d(LOG_TAG, "Load from URL Failed");
+                replaceYouTubeVideo();
+            }
 
         });
 
@@ -120,8 +159,13 @@ public class MovieDetailsActivity extends YouTubeBaseActivity {
                     @Override
                     public void onInitializationSuccess(YouTubePlayer.Provider provider,
                                                         YouTubePlayer youTubePlayer, boolean b) {
+                        progressBar.setVisibility(View.GONE);
+                        if(m.getVoteAverage() >= 6.5){
+                            youTubePlayer.loadVideo(videoKey);
+                        }else{
+                            youTubePlayer.cueVideo(videoKey);
+                        }
 
-                        youTubePlayer.cueVideo(videoKey);
                     }
                     @Override
                     public void onInitializationFailure(YouTubePlayer.Provider provider,
@@ -130,5 +174,32 @@ public class MovieDetailsActivity extends YouTubeBaseActivity {
                     }
                 });
 
+    }
+
+    // replaces YouTube video with the backdrop of the movie
+    private void replaceYouTubeVideo(){
+
+        progressBar.setVisibility(View.VISIBLE);
+        youTubePlayerView.setVisibility(View.GONE);
+
+        Toast.makeText(getApplicationContext(), "No Movie Trailer Found", Toast.LENGTH_LONG).show();
+
+        Glide.with(getApplicationContext())
+                .load(m.getBackdropPath())
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        progressBar.setVisibility(View.VISIBLE);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        progressBar.setVisibility(View.GONE);
+                        noVideoIV.setVisibility(View.VISIBLE);
+                        return false;
+                    }
+                })
+                .into(noVideoIV);
     }
 }
